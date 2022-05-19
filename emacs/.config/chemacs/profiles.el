1c717e9961398f59507b9eb0ee5eb43b1f858e9d
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2022-05-19 19:18:55 -0300

    Add Spacemacs profile for chemacs

--- emacs/.config/chemacs/profiles.el
+++ emacs/.config/chemacs/profiles.el
@@ -1,1 +1,3 @@
-(("doom"      . ((user-emacs-directory . "~/.emacs-distros/doom"))))
+(("space" . ((user-emacs-directory . "~/.emacs-distros/spacemacs")
+	     (env . (("SPACEMACSDIR" . "~/.config/spacemacs/")))))
+ ("doom"      . ((user-emacs-directory . "~/.emacs-distros/doom"))))
