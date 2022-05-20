d3e61c5e00e72db34d54ca1bc5c3672a2aaceba3
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2022-05-20 16:07:55 -0300

    Clone doom/defaults module into Spacemacs module

--- emacs/.config/doom/modules/private/spacemacs/init.el
+++
@@ -1,7 +0,0 @@
-;;; init.el --- Provides the Spacemacs ergonomics that I like but within Doom  -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2022  Matheus Abegg
-
-;; Author: Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
-;; Keywords: spacemacs, doom, ergonomics, keymaps,
-;;
