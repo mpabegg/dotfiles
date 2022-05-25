1dc571284b962a7db88365b469a9ae83de1e5303
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2022-05-25 17:13:50 -0300

    Small tweaks to code format and comments

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -20,13 +20,6 @@
   (treemacs-define-RET-action 'file-node-open #'treemacs-visit-node-ace)
   (treemacs-define-RET-action 'file-node-closed #'treemacs-visit-node-ace))
 
-(after! winum
-  (map! :leader
-        "0" #'treemacs-select-window
-        "1" #'winum-select-window-1
-        "2" #'winum-select-window-2
-        "3" #'winum-select-window-3
-        "4" #'winum-select-window-4))
 
 (map! :mode dired-mode
       :n "h" #'dired-up-directory
@@ -89,6 +82,11 @@
 
 (after! winum
   (map! :leader
+        "0" #'treemacs-select-window
+        "1" #'winum-select-window-1
+        "2" #'winum-select-window-2
+        "3" #'winum-select-window-3
+        "4" #'winum-select-window-4
         "w m" #'spacemacs/toggle-maximize-buffer
         "w 2" #'spacemacs/window-split-double-columns
         "w 3" #'spacemacs/window-split-in-three
@@ -96,15 +94,12 @@
 
 (defun spacemacs/alternate-buffer (&optional window)
   "Switch back and forth between current and last buffer in the
-current window.
-
-If `spacemacs-layouts-restrict-spc-tab' is `t' then this only switches between
-the current layouts buffers."
+current window."
   (interactive)
   (cl-destructuring-bind (buf start pos)
         (or (cl-find (window-buffer window) (window-prev-buffers)
-                     :key #'car :test-not #'eq)
-            (list (other-buffer) nil nil))
+                        :key #'car :test-not #'eq)
+                (list (other-buffer) nil nil))
     (if (not buf)
         (message "Last buffer not found.")
       (set-window-buffer-start-and-point window buf start pos))))
