9e4bfdcb67272c2c0d5a31765084d970e4269324
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2022-06-01 20:18:32 -0300

    Update doom packages

--- emacs/.config/doom/custom.el
+++ emacs/.config/doom/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("82ef0ab46e2e421c4bcbc891b9d80d98d090d9a43ae76eb6f199da6a0ce6a348" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "c614d2423075491e6b7f38a4b7ea1c68f31764b9b815e35c9741e9490119efc0" "196df8815910c1a3422b5f7c1f45a72edfa851f6a1d672b7b727d9551bb7c7ba" default))
  '(package-selected-packages '(auto-highlight-symbol)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
