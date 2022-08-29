93d0d3f19c16cd4d2213367203eee2f4f452e4f2
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2022-08-29 09:46:34 -0300

    mpa-emacs import

--- emacs/.config/chemacs/profiles.el
+++ emacs/.config/chemacs/profiles.el
@@ -1,3 +1,3 @@
 (("doom"      . ((user-emacs-directory . "~/.emacs-distros/doom/")))
- ("rational"  . ((user-emacs-directory . "~/.emacs-distros/rational/")
-                 (env . (("RATIONAL_EMACS_HOME" . "~/.config/rational-emacs/"))))))
+ ("mpa"       . ((user-emacs-directory . "~/.emacs-distros/crafted/")
+                 (env . (("CRAFTED_EMACS_HOME" . "~/.config/mpa-emacs/"))))))
