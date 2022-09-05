b93bce4eef4df3272d56dbd3d1f2deed2e0310e0
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2022-09-05 10:34:23 -0300

    cleanpu null

--- nvim/.config/nvim/lua/mpa/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/mpa/lsp/null-ls.lua
@@ -1,27 +1,22 @@
-local null_ls_status_ok, null_ls = pcall(require, "null-ls")
-if not null_ls_status_ok then
-  return
-end
-
+local null_ls = require "null-ls"
 local formatting = null_ls.builtins.formatting
 -- local diagnostics = null_ls.builtins.diagnostics
 
-local conditional = function(fn)
+local rubocop = function()
   local utils = require("null-ls.utils").make_conditional_utils()
-  return fn(utils)
+  local bundled = utils.root_has_file "Gemfile"
+  local opts = bundled and {
+    command = "bundle",
+    args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.formatting.rubocop._opts.args),
+  } or {}
+
+  return formatting.rubocop.with(opts)
 end
 
 null_ls.setup({
   debug = false,
   sources = {
     formatting.stylua,
-    conditional(function(utils)
-      return utils.root_has_file "Gemfile"
-          and null_ls.builtins.formatting.rubocop.with({
-            command = "bundle",
-            args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.formatting.rubocop._opts.args),
-          })
-        or null_ls.builtins.formatting.rubocop
-    end),
+    rubocop(),
   },
 })
