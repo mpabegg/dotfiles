e29c6aa2516193ba801fd9c5fa8309558efcf94a
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2022-09-05 10:35:35 -0300

    colors

--- nvim/.config/nvim/lua/mpa/colors.lua
+++ nvim/.config/nvim/lua/mpa/colors.lua
@@ -1,7 +1,7 @@
 local colorscheme = "tokyonight"
 
 vim.o.background = "dark"
-vim.g.tokyonight_style = "night"
+vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_trasnparent = true
 
 local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
