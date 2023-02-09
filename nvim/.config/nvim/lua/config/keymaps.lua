8dc10003db40c26577f2f6c5af1f42cd4e789e5c
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2023-02-09 11:42:06 -0300

    Bunch of stuff

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<C-h>", vim.cmd.NvimTmuxNavigateLeft)
+vim.keymap.set("n", "<C-j>", vim.cmd.NvimTmuxNavigateDown)
+vim.keymap.set("n", "<C-k>", vim.cmd.NvimTmuxNavigateUp)
+vim.keymap.set("n", "<C-l>", vim.cmd.NvimTmuxNavigateRight)
+vim.keymap.set("n", "<C-\\>", vim.cmd.NvimTmuxNavigateLastActive)
+vim.keymap.set("n", "<C-Space>", vim.cmd.NvimTmuxNavigateNext)
