067d218c2796d24a69bb6dc4a78a3de5e53af048
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2023-05-25 09:25:43 -0300

    I'm not using emacs anymore

--- emacs/.config/chemacs/profiles.el
+++
@@ -1,3 +0,0 @@
-(("doom"      . ((user-emacs-directory . "~/.emacs-distros/doom/")))
- ("mpa"       . ((user-emacs-directory . "~/.emacs-distros/crafted/")
-                 (env . (("CRAFTED_EMACS_HOME" . "~/.config/mpa-emacs/"))))))
