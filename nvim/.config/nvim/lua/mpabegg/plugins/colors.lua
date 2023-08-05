e333ad578517e2c9696b70eee3d0099f5c2d0a83
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2023-08-05 10:41:39 -0300

    Add priority loading to colorscheme

--- nvim/.config/nvim/lua/mpabegg/plugins/colors.lua
+++ nvim/.config/nvim/lua/mpabegg/plugins/colors.lua
@@ -1,6 +1,8 @@
 return {
   'catppuccin/nvim',
   name = 'catppuccin',
+  lazy = false,      -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000,   -- make sure to load this before all the other start plugins
   init = function()
     require('catppuccin').setup({
       flavour = 'frappe',
