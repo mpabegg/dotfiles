97966b0765ae3489f4da2c111ae665c2a7b987de
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2023-08-08 09:04:07 -0300

    Ignore missing fields diagnostic on catppuccin setup

--- nvim/.config/nvim/lua/mpabegg/plugins/colors.lua
+++ nvim/.config/nvim/lua/mpabegg/plugins/colors.lua
@@ -2,6 +2,7 @@ return {
   'catppuccin/nvim',
   name = 'catppuccin',
   init = function()
+    ---@diagnostic disable-next-line: missing-fields
     require('catppuccin').setup({
       flavour = 'frappe',
     })
