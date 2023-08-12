62819c635eae9b71a1b9ac105c0803a561f6ed87
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2023-08-12 17:20:41 -0300

    Stop lazy notification

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -25,4 +25,9 @@ require('lazy').setup({
   install = {
     colorscheme = { 'catppuccin frappe' },
   },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = false, -- get a notification when changes are found
+  },
 })
