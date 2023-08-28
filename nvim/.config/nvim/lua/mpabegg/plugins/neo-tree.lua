0af90dcb901324114ed014e634a9a5fcdd20c7cb
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2023-08-28 10:32:00 -0300

    nvim: window-picker to integrate with neo-tree

--- nvim/.config/nvim/lua/mpabegg/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/mpabegg/plugins/neo-tree.lua
@@ -63,6 +63,15 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    {
+      's1n7ax/nvim-window-picker',
+      name = 'window-picker',
+      event = 'VeryLazy',
+      version = '2.*',
+      config = function()
+        require('window-picker').setup()
+      end,
+    },
   },
   opts = config,
 }
