91984123bb9fd281436a06728f5eeecf0a4d2012
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2025-11-13 19:45:46 -0300

    Git sings and better fold

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,35 @@ require'mpa.folds'
 require'mpa.colors'
 require'mpa.git'
 
+local icons = require('mpa.icons')
+
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = icons.diagnostics.error,
+      [vim.diagnostic.severity.WARN]  = icons.diagnostics.warn,
+      [vim.diagnostic.severity.HINT]  = icons.diagnostics.hint,
+      [vim.diagnostic.severity.INFO]  = icons.diagnostics.info,
+    }
+  },
+  virtual_text = false,
+  update_in_insert = false,
+  underline = false,
+  severity_sort = true,
+  float = {
+    focusable = true,
+    style = 'minimal',
+    border = 'rounded',
+    source = 'always',
+    header = '',
+    prefix = '',
+  },
+})
+
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+
 vim.pack.add { 
   { src = "https://github.com/stevearc/oil.nvim" },
 }
--- nvim/.config/nvim/lua/mpa/git.lua
+++ nvim/.config/nvim/lua/mpa/git.lua
@@ -4,15 +4,22 @@ require('gitsigns').setup({
   current_line_blame_opts = {
     virt_text = true,
   },
-  signs = {
-    add = { text = '▎' },
-    change = { text = '▎' },
-    delete = { text = '' },
-    topdelete = { text = '' },
-    changedelete = { text = '▎' },
-    untracked = { text = '▎' },
-  },
-  on_attach = function(buffer)
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      signs_staged = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+      },
+ on_attach = function(buffer)
     local gs = package.loaded.gitsigns
 
     local function map(mode, l, r, desc)
