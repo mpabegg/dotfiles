f8c5b38eb04b43369b210ff66ea5d2815005ed23
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2025-11-13 21:16:34 -0300

    Add tmux navigation

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,6 +4,17 @@ require'mpa.colors'
 require'mpa.git'
 require'mpa.lang'
 
+vim.pack.add({ { src = 'https://github.com/alexghergh/nvim-tmux-navigation' } })
+require('nvim-tmux-navigation').setup({
+  disable_when_zoomed = true,
+  keybindings = {
+    left = '<C-h>',
+    down = '<C-j>',
+    up = '<C-k>',
+    right = '<C-l>',
+  },
+})
+
 vim.pack.add{{src="https://github.com/folke/snacks.nvim"}}
 vim.pack.add{{src="https://github.com/folke/ts-comments.nvim"}}
 vim.pack.add{{src="https://github.com/folke/lazydev.nvim"}}
